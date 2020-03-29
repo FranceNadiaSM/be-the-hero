71ad5d0ff80ab8cd7ded9e092cfd820afb22805a
FranceNadia <frannasanmo@gmail.com>
2020-03-28 21:26:41 -0300

    Front-end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+// JSX (JavaScript XML) -> O HTML está integrado dentro do JavaScript
+
+function App() {
+    return (
+      <Routes />
+  );
+}
+
+export default App;
